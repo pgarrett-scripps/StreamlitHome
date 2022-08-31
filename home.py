d4777eba0a34618271019faef9001285647a7620
pgarrett <pgarrett@scripps.edu>
2022-08-31 12:24:53 -0700

    first commit

---
+++ home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.image('http://fields.scripps.edu/yates/wp/wp-content/uploads/2016/05/Picture1.png')
+st.title('Yates Lab Applications')
+
+
+st.subheader("Public")
+st.write("FASTA Suite [link](https://pgarrett-scripps-fasta-filter-streamlit-home-teygff.streamlitapp.com/)")
+st.write("Peptide Modification Visualizer [link](https://pgarrett-scripps-peptidemodificationvis-modification-app-ctqser.streamlitapp.com/)")
+st.write("Fragment Ion Visualizer [link](https://pgarrett-scripps-fragmentionvisualizer-main-qbdch1.streamlitapp.com/)")
+st.write("Spectral Library Generator [link](https://pgarrett-scripps-peptidemodificati-spectral-library-app-6gepgv.streamlitapp.com/)")
+#st.write("GlycoMSQuantpy [link]()")
+
+st.subheader("Private (Scripps VPN Required)")
+st.write("Search Engine [link](http://marlin:8501)")
+st.write("SQT-Rescore [link](http://marlin:8502)")
+st.write("")
+st.write("")
+st.write("contact: pgarrett@scripps.edu")
