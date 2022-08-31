93049fe33792c86937f6e1b59c77ee8fb425437a
pgarrett <pgarrett@scripps.edu>
2022-08-31 13:43:04 -0700

    made image local

--- home.py
+++ home.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-st.image('http://fields.scripps.edu/yates/wp/wp-content/uploads/2016/05/Picture1.png')
+st.image('yates_lab.png')
 st.title('Yates Lab Applications')
 
 
 st.subheader("Public")
-st.write("FASTA Suite [link](https://pgarrett-scripps-fasta-filter-streamlit-home-teygff.streamlitapp.com/)")
+st.write("FASTA Suite [link](https://pgarrett-scripps-fastasuite-home-v5ftop.streamlitapp.com/)")
 st.write("Peptide Modification Visualizer [link](https://pgarrett-scripps-peptidemodificationvis-modification-app-ctqser.streamlitapp.com/)")
 st.write("Fragment Ion Visualizer [link](https://pgarrett-scripps-fragmentionvisualizer-main-qbdch1.streamlitapp.com/)")
 st.write("Spectral Library Generator [link](https://pgarrett-scripps-peptidemodificati-spectral-library-app-6gepgv.streamlitapp.com/)")
